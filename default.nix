f9628ef65a61041e883c176588053b255de57402
jaminW55 <133602605+jaminW55@users.noreply.github.com>
2023-11-20 17:08:52 -0500

    Update default.nix
    added gazou_wayland

--- default.nix
+++ default.nix
@@ -3,4 +3,5 @@ with import <nixpkgs> {};
 {
   gazou = pkgs.callPackage ./gazou.nix {};
   tachidesk = pkgs.callPackage ./tachidesk.nix {};
+  gazou-wayland = pkgs.callPackage ./gazou-wayland.nix {};
 }
